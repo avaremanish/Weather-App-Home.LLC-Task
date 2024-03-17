7240d83469e817d9ea6f4970a08b8b671d5d4d42
Manish Kumar <95077592+avaremanish@users.noreply.github.com>
2024-03-17 15:24:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,7 @@ import { HomePage } from "./Pages/Home/HomePage";
 import "./App.css"
 
 function App() {
-  const [weather, setWeather] = useState(null);
-  const [location, setLocation] = useState("india");
-
-  const apiKey = "1607394c84102ee7158d0ab45d78d0ad"; // Replace with your own API key
-
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
-    );
-    const data = await response.json();
-    setWeather(data);
   
-  };
-  
-
-
-  useEffect(() => {
-    handleSearch();
-  }, []);
-
-  // if (!weather) {
-  //   return <div>Loading...</div>;
-  // }
 
   return (
     <div className="App">
--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./homePage.css"
 import { HiBars2 } from "react-icons/hi2";
 import { FiSearch } from "react-icons/fi";
@@ -7,6 +7,38 @@ import IMG from "../../assets/graph.jpg"
 
 
 export const HomePage = () => {
+     const [query, setQuery] = useState({ q: "berlin" });
+     const [units, setUnits] = useState("metric");
+     const [weather, setWeather] = useState(null);
+
+     useEffect(() => {
+       const fetchWeather = async () => {
+         const message = query.q ? query.q : "current location.";
+
+         toast.info("Fetching weather for " + message);
+
+         const data = await getFormattedWeatherData({ ...query, units }).then(
+           (data) => {
+             toast.success(
+               `Successfully fetched weather for ${data.name}, ${data.country}.`
+             );
+             setWeather(data);
+           }
+         );
+         console.log(data);
+       };
+
+       fetchWeather();
+     }, [query, units]);
+
+     const formatBackground = () => {
+       if (!weather) return " from-cyan-700 to-blue-700";
+       const threshold = units === "metric" ? 20 : 50;
+       if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+       return "from-yellow-700 to-orange-700";
+     };
+
   return (
     <>
       <div className="Home-page">
